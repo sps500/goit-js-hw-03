80cfd35c7db8cee213b16633d30288b8b19b84a0
sps500 <148262907+sps500@users.noreply.github.com>
2024-01-22 17:58:39 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
-
-  if (!isNaN(contentValue) && !isNaN(paddingValue) && !isNaN(borderValue)) {
-      const width = contentValue + 2 * paddingValue + 2 * borderValue;
-      return width;
-  } else {
-      console.error("Invalid input values");
-      return null;
+function filterArray(numbers, value) {
+  const filteredArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
+    }
   }
+
+  return filteredArray;
 }
-  console.log(getElementWidth("50px", "8px", "4px"));   
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px"));  
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
